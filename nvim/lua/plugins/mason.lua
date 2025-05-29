d2b495f897fa29870945b288e01650c47774facf
Alessio Englert <alessio@alessios-macbook-pro.home>
2025-05-29 15:12:37 +0200

    feat: added mason.lua

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "dart-debug-adapter",
+      "dcm",
+      "eslint_d",
+      "lua-language-server",
+      "prettierd",
+      "shfmt",
+      "stylua",
+      "typescript-language-server",
+      "vue-language-server",
+    },
+  },
+}
