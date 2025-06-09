3c57e46f47a74f863698f4eb828c4cd41b6fb4fa
Alessio Englert <alessio@alessios-macbook-pro.home>
2025-06-09 07:53:26 +0200

    feat: updated javascript language server

--- nvim/lua/plugins/lsp/javascript.lua
+++ nvim/lua/plugins/lsp/javascript.lua
@@ -18,5 +18,19 @@ return {
         },
       },
     })
+
+    require("lspconfig").ts_ls.setup({
+      init_options = {
+        plugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = vim.fn.stdpath("data")
+              .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+            languages = { "vue" },
+          },
+        },
+      },
+      filetypes = { "vue", "typescript", "javascript", "typescriptreact", "javascriptreact" },
+    })
   end,
 }
