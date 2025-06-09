636bfd913a9e7c8a1e083a776e270930ed07c26f
Alessio Englert <alessio@alessios-macbook-pro.home>
2025-06-09 16:33:41 +0200

    fix: removed comment code

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,5 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- vim.cmd([[colorscheme vscode]])
 vim.opt.termguicolors = true
 vim.o.textwidth = 120
