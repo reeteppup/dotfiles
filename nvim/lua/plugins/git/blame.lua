43e6d97fda437d76ee883c7ee889716375af2170
Alessio Englert <alessio@alessios-macbook-pro.home>
2025-06-19 10:59:12 +0200

    feat: added veryLazy event to different plugins for faster loading time

--- nvim/lua/plugins/git/blame.lua
+++ nvim/lua/plugins/git/blame.lua
@@ -1,6 +1,7 @@
 return {
   {
     "f-person/git-blame.nvim",
+    event = "VeryLazy",
     opts = {
       enabled = true,
       message_template = " <summary> • <date> • <author> • <<sha>>",
--- nvim/lua/plugins/git/diffView.lua
+++ nvim/lua/plugins/git/diffView.lua
@@ -1,5 +1,6 @@
 return {
   "sindrets/diffview.nvim",
+  event = "VeryLazy",
   dependencies = { "nvim-lua/plenary.nvim" }, -- plenary is required for diffview
   config = function()
     require("diffview").setup()
